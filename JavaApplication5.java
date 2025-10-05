2052f8cea5db4df17b086622299aed909ed35d30
Abdulelah-Ayman <s447007544@uqu.edu.sa>
2025-10-06 02:09:41 +0300

    Add files via upload

---
+++ JavaApplication5.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author عبدالإله
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int day=in.nextInt();
+        switch(day){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            System.out.println("Week day");
+            break;
+        case 6:
+        case 7:
+            System.out.println("Weekend");
+            break;    
+        
+        
+        
+        
+        
+        
+        
+        }
+      
+        
+        
+    }
+    
+}
